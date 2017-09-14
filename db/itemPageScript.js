360a974ef6c7c93870cf1e9213acea6770820868
Inomares <f-a-k-i-e@hotmail.com>
2017-09-14 23:31:22 +0200

    Forgot to also update the script file...

--- db/itemPageScript.js
+++ db/itemPageScript.js
@@ -2,6 +2,8 @@ var dontUpdateHash = false
 var attachedPopups = {}
 var testNode = null
 
+var fileHost = "http://res.eveprobe.ccpgames.com/"
+
 function getParameterByName(name) {
 
 	var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
@@ -24,7 +26,7 @@ function selectTab(evt, tabName) {
 		let node = nodes[i]
 		node.classList.remove("selected")
 	}
-	let contents = document.getElementById(tabName)
+	let contents = document.getElementById("content_" + tabName)
 	contents.classList.remove("inactive")
 	evt.currentTarget.classList.add("selected")
 }
@@ -86,17 +88,153 @@ function getAttributeDescription(attrID) {
 function getAttributeIcon(attrID) {
 	let attribute = dynamicData.attributes[attrID]
 	if (attribute != null) {
-		return attribute.icon
+		return fileHost + attribute.icon
+	}
+	return null
+}
+
+function getTypeName(typeID, safe) {
+	safe = safe || true
+	let type = dynamicData.types[typeID]
+	if (type != null) {
+		return type.name
+	}
+	if (safe) {
+		return "Invalid Item (" + typeID + ")"
 	}
 	return null
 }
 
 function getTypeURL(typeID) {
-	let type = dynamicData.types[typeID]
-	if (type != null) {
-		return "/db/type/" + type.url
+	let itemName = getTypeName(typeID, false)
+	if (itemName != null) {
+		return "/db/type/" + typeID + "-" + itemName.replace(/[^A-Za-z0-9 ]/g, "").replace(/ +/g, "-")
 	}
 	return null
+}
+
+function getTypeIcon(typeID) {
+	let type = dynamicData.types[typeID]
+	if (type != null) {
+		return fileHost + type.icon
+	}
+	return "https://image.testeveonline.com/type/1_64.png"
+}
+
+function getTypeGroup(typeID) {
+	let type = dynamicData.types[typeID]
+	if (type != null) {
+		return type.group
+	}
+	return null
+}
+
+function isTypePublished(typeID) {
+	let type = dynamicData.types[typeID]
+	if (type != null) {
+		return type.pub
+	}
+	return false
+}
+
+// Attributes: Regular - Unpublished - full (non-numerical)
+// Skins: Show only tq skins, include unpublished
+// Requirements: Auto expand skill prereqs
+function toggleSettings() {
+	alert('Settings coming soon! Probably.')
+}
+
+function setupSkillLinks() {
+	let t1 = performance.now()
+	var nodes = document.getElementsByClassName("skillLink")
+	for (var i=0; i < nodes.length; i++) {
+		var node = nodes[i]
+		let typeID = node.getAttribute("typeid")
+		let itemURL = getTypeURL(typeID)
+		let itemName = getTypeName(typeID, true)
+		if (itemURL != null) {
+			node.href = itemURL
+		}
+		node.innerHTML = itemName + " " + node.innerHTML
+	}
+	let t2 = (performance.now() - timeLoadStart).toFixed(3)
+	console.log("Set up skill links in " + timeLoadEnd + " ms.")
+}
+
+function shouldShowSkin(skin) {
+	return true
+}
+
+function setupSkins() {
+	var skinNodes = []
+	var skinParent = document.getElementById("content_skins")
+	skinParent.classList.add("dynamicDisabled")
+	skinParent.innerHTML = "" //Clear existing content
+	for (var skinIdx in typeData.skins) {
+		var skin = typeData.skins[skinIdx]
+		if (shouldShowSkin(skin)) {
+			var aParent = document.createElement("a")
+			aParent.href = getTypeURL(skin.typeID)
+			
+			var divParent = document.createElement("div")
+			divParent.classList.add("skinbox")
+			aParent.appendChild(divParent)
+			
+			var aImage = document.createElement("img")
+			aImage.src = getTypeIcon(skin.typeID)
+			aImage.classList.add("skinIcon")
+			divParent.appendChild(aImage)
+			
+			var aDiv = document.createElement("div")
+			aDiv.classList.add("skinDescriptor")
+			divParent.appendChild(aDiv)
+			
+			var spanName = document.createElement("span")
+			spanName.classList.add("skinName")
+			spanName.innerHTML = getTypeName(skin.typeID, true)
+			aDiv.appendChild(spanName)
+			
+			var spanNotes = document.createElement("span")
+			spanNotes.classList.add("skinNotes")
+			spanNotes.innerHTML = "<b>Source:</b> Unknown"
+			aDiv.appendChild(spanNotes)
+			
+			skinNodes.push(aParent)
+		}
+	}
+	if (skinNodes.length > 0) {
+		for (var i=0; i < skinNodes.length; i++) {
+			var parentNode = skinNodes[i]
+			skinParent.appendChild(parentNode)
+		}
+		skinParent.classList.remove("dynamicDisabled")
+		document.getElementById("button_skins").classList.remove("dynamicDisabled")
+	}	
+}
+
+function shouldShowAttribute(attributeID) {
+	return true
+}
+
+function getAttributeOrder(categoryID) {
+	if (dynamicData.attributeOrders[categoryID] != null) {
+		return dynamicData.attributeOrders[categoryID]
+	}
+	return dynamicData.attributeOrders["default"]
+}
+
+function setupAttributes() {
+	var attrNodes = []
+	var attrParent = document.getElementById("content_attributes")
+	attrParent.classList.add("dynamicDisabled")
+	attrParent.innerHTML = "" //Clear existing content
+	var attrOrder = getAttributeOrder(typeData.category)
+	for (var attrHeaderName in attrOrder) {
+		var attributeGroups = attrOrder[attrHeaderName]
+		console.log(attrHeaderName)
+		console.log(attributeGroups)
+	}
+	attrParent.classList.remove("dynamicDisabled")
 }
 
 window.onload = function(){
@@ -105,6 +243,13 @@ window.onload = function(){
 		var node = nodes[i]
 		node.classList.remove("noscripthidden")
 	}
+	
+	/* fixTraitsLinks() */
+	setupAttributes()
+	setupSkillLinks()
+	setupSkins()
+	
+	
 	var useDefaultTab = true
 	if (window.location.hash.length > 1) {
 		let desiredTabName = window.location.hash.slice(1)
@@ -116,10 +261,14 @@ window.onload = function(){
 	}
 	if (useDefaultTab) {
 		nodes = document.getElementsByClassName("tablink")
-		if (nodes.length > 0) {
-			dontUpdateHash = true
-			nodes[0].click();
-			dontUpdateHash = false
+		for (var i=0; i < nodes.length; i++) {
+			var node = nodes[i]
+			if (!node.classList.contains("dynamicDisabled")) {
+				dontUpdateHash = true
+				node.click();
+				dontUpdateHash = false
+				break
+			}
 		}
 	}
 }
