b399b5f362743a5b8433de1fa6eb0b16d09193e3
Inomares <f-a-k-i-e@hotmail.com>
2019-01-22 12:53:24 +0100

    Fixing auroras

--- auroras.js
+++ auroras.js
@@ -157,7 +157,7 @@ function loadPlanets(data) {
 function reloadPlanets() {
     updateStatusText(true)
     if (auroraData == null) {
-        $.getJSON("https://beta1.hoboleaks.space/auroras.json", function (j) {
+        $.getJSON("https://hoboleaks.space/auroras.json", function (j) {
             initData(j, true)
         })
     } else {
